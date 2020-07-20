463906539fef092c7ba141f0bec8f8ed0ae3d6ad
Badkul'S <namanbadkul77@gmail.com>
2020-07-21 01:51:25 +0530

    Show Images

--- app/src/main/java/com/example/image_database/ShowImages.java
+++ app/src/main/java/com/example/image_database/ShowImages.java
@@ -1,14 +1,77 @@
 package com.example.image_database;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShowImages extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+    List<ModelImage> imageList;
+    ModelImage modelImage;
+    LinearLayoutManager linearLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_images);
+        recyclerView = findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        imageList = new ArrayList<>();
+        myAdapter = new MyAdapter(this,imageList);
+        recyclerView.setAdapter(myAdapter);
+
+        fetchImages();
+    }
+
+    private void fetchImages() {
+        String url="https://simplyfied.co.in/Test/fetchImages.php";
+        StringRequest request=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject=new JSONObject(response);
+                    String success=jsonObject.getString("success");
+
+                    JSONArray jsonArray=jsonObject.getJSONArray("data");
+                    if (success.equals("1"))
+                    {
+                        for(int i=0;i<jsonArray.length();i++) {
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            String id=object.getString("id");
+                            String imageurl=object.getString("image");
+                            String u = "https://simplyfied.co.in/Test/images/"+imageurl;
+                            modelImage=new ModelImage(id,u);
+                            imageList.add(modelImage);
+                            myAdapter.notifyDataSetChanged();
+                        }
+                        }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ShowImages.this, ""+error, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
